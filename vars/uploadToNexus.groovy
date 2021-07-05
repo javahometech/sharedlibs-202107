4f0f06553aaa42bc170b6222bd8cf98791552e9a
javahometech <kammana.hari@gmail.com>
2021-07-05 08:23:29 +0530

    Update uploadToNexus.groovy

--- vars/uploadToNexus.groovy
+++ vars/uploadToNexus.groovy
@@ -2,7 +2,8 @@ def call(nexusRepo){
     def pom = readMavenPom file: 'pom.xml'
     def repo = nexusRepo
     def groupId = pom.groupId
-  nexusArtifactUploader artifacts: [[artifactId: artifactId, classifier: '', file: "target/${artifactId}-${pom.version}.war", type: 'war']], 
+    def artifactId = pom.artifactId
+    nexusArtifactUploader artifacts: [[artifactId: artifactId, classifier: '', file: "target/${artifactId}-${pom.version}.war", type: 'war']], 
       credentialsId: 'nexus3', 
       groupId: groupId, 
       nexusUrl: '172.31.43.180:8081', 
